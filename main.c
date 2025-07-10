6332c566401da262c56c332cd1c03d814048e52e
Laureano Falcon <100383079+youngfinn000@users.noreply.github.com>
2025-07-10 05:05:02 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,78 @@
 #include <stdlib.h>
 #include <string.h>
 
+///
+int cargarArregloDin(int dim, int **arregloDinamico)
+{
+    (*arregloDinamico)=(int*) malloc(dim * sizeof(int));
+    char conf='s';
+    int validos=0;
+    while (conf=='s' && validos<dim)
+    {
+        (*arregloDinamico)[validos]=cargarArregloDin();
+    }
+    
+    return validos;
+}
+
+Vacunas* agregarUnElementoAlArreglo (Vacunas *arreglo, int *validos)
+{
+    (*validos)++; // aumentamos el tamano logico
+    arreglo = (Vacunas*) realloc (arreglo,(*validos)*sizeof(Vacunas));
+
+    printf("\nNueva vacuna a agregar...\n");
+    arreglo[(*validos)-1] = cargarUnaVacuna();  
+
+    FILE* archi = fopen(nombreArchi, "ab");
+
+    if (archi!=NULL)
+    {
+        fwrite(&arreglo[(*validos)-1], sizeof(Vacunas), 1, archi);
+        fclose(archi);
+    }
+
+    return arreglo;
+}
+
+int pasarDePilaAarreglo (Pila aCopiar, int arreglo[], int dim)
+{
+    int validos=0;
+    Pila aux;
+    inicpila(&aux);
+
+    while(!pilavacia(&aCopiar) && validos<dim)
+    {
+        if (tope(&aCopiar)>250 && tope(&aCopiar)<350)
+        {
+            validos = insertarEnArregloOrdenada(arreglo, validos, desapilar(&aCopiar));
+        }
+        else
+        {
+            apilar(&aux, desapilar(&aCopiar));
+        }
+    }
+
+    return validos;
+}
+
+
+int insertarEnArregloOrdenada (int arreglo[], int validos, int aAgregar)
+{
+    int j=validos-1;
+
+    while(j>=0 && arreglo[j]>aAgregar)
+    {
+        arreglo[j+1]=arreglo[j];
+        j--;
+    }
+
+    arreglo[j+1]= aAgregar;
+
+    return validos+1;
+}
+
+///
+
 
 #define archivosNum "numeros.bin"
 
@@ -12,7 +84,7 @@ typedef struct
     char nombreYapellido [30];
     int edad;
     int anio;
-//año que cursa, recordar que no podemos utilizar la ñ para definir variables
+//aÃ±o que cursa, recordar que no podemos utilizar la Ã± para definir variables
 } stAlumno;
 
 /// PROTOTIPADO ///////////////////////////
